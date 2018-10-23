3877f698ef0b44167dfc85e9f125a070eb8f7507
WHU-YH-jx <1092949763@qq.com>
2018-10-23 19:39:02 +0800

    HomeWork 6

---
+++ HomeWork6/HomeWork6Tests/OrderServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HomeWork6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    { 
+        OrderService Oporder = new OrderService();
+        OrderDetails first = new OrderDetails(3, "C#", "Language", 12, 12);
+        OrderDetails second = new OrderDetails(4, "Java", "Class", 21, 21);
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            Oporder.AddOrder(first);
+            Oporder.AddOrder(second);
+            Assert.IsTrue("C#" == Oporder.OrderList[0].CustomerName);
+        }
+
+        [TestMethod()]
+        public void DeleteOrderTest()
+        {
+            Oporder.AddOrder(first);
+            Oporder.AddOrder(second);
+            Oporder.DeleteOrder(3);
+            Assert.IsTrue(1== Oporder.OrderList.Count);
+        }
+
+        [TestMethod()]
+        public void DeleteOrderTest1()
+        {
+            Oporder.AddOrder(first);
+            Oporder.AddOrder(second);
+            Oporder.DeleteOrder("Java");
+            Assert.IsTrue(1 == Oporder.OrderList.Count);
+        }
+
+        [TestMethod()]
+        public void ChangeOrderTest()
+        {
+            Oporder.AddOrder(first);
+            Oporder.AddOrder(second);
+            Oporder.ChangeOrder("C#", 3);
+            Assert.IsTrue(3 == Oporder.OrderList[0].GoodsNumber);
+        }
+
+        [TestMethod()]
+        public void ChangeOrderTest1()
+        {
+            Oporder.AddOrder(first);
+            Oporder.AddOrder(second);
+            Oporder.ChangeOrder("Language", 3);
+            Assert.IsTrue(3 == Oporder.OrderList[0].GoodsNumber);
+        }
+    }
+}
